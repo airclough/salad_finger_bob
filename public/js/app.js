befc71d7484a2920770f9296ad34816428e22068
Robby Fairclough <robby.fairclough@gmail.com>
2015-06-09 23:26:23 -0700

    create controller

--- public/js/app.js
+++ public/js/app.js
@@ -4,9 +4,46 @@ define(
   function( events, Router ) {
     'use strict';
 
+    var controller = {
+      init: function( steps ) {
+        this.steps = steps;
+        this.router = new Router( {}, this.steps );
+        this.appView = appView;
+
+        this._subscribe()
+        this.appView.init();
+      },
+
+      _subscribe: function() {
+        events.on( 'router:navigate', this._triggerNavigate, this );
+        return this;
+      },
+
+      _triggerNavigate: function( view ) {
+        console.log( view );
+      }
+    };
+
     return {
       init: function() {
-        console.log( events, Router );
+        this._getSteps();
+      },
+
+      _getSteps: function() {
+        var that = this;
+        var req = $.ajax({
+          url     : url,
+          type    : 'GET',
+          dataType: 'json'
+        });
+
+        req.done( function( data, textStatus, jqXHR ) {
+          controller.init( data );
+        });
+
+        req.fail( function( jqXHR, textStatus, errorThrown ) {
+          console.log( 'Salad Finger Fail' );
+        });
       }
     };
   }
